6f699d47e582e247443050ad662b21ac87911135
unknown <mangotest1965@yahoo.com>
2026-03-25 16:43:14 +0000

    commented out router option

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory} from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Project from '../views/Project.vue'
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/",
+            component: () => import('../views/Home.vue')
+        },
+        {
+            path: "/about",
+            component: () => import('../views/About.vue')
+        },
+        {
+            path: "/projects",
+            component: () => import('../views/Project.vue')
+        },
+        {
+            path: "/contact",
+            component: () => import('../views/Contact.vue')
+        },
+        {
+            path: "/profile",
+            component: () => import('../views/Profile.vue')
+        },
+         {
+            path: "/settings",
+            component: () => import('../views/Settings.vue'),
+            props: true
+        },
+        // {
+        //     path: "/:pathMatch(.*)*", // Catch-all route for unknown paths
+        //     component: Home      // Redirect to home 
+        // }
+    ] 
+})
+
+export default router
